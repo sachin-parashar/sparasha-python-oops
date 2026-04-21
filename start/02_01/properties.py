38c3e4ef3adc5c40028c1ae5f52bb09b9752d6f9
sachin-parashar <87593568+sachin-parashar@users.noreply.github.com>
2026-04-21 13:27:43 +0530

    Add Rectangle class with base and height properties

--- start/02_01/properties.py
+++ start/02_01/properties.py
@@ -1,3 +1,13 @@
 """
 Python class properties
 """
+class Rectangle:
+   def __init__(self,base,height):
+     self.base = base
+     self.height = height
+
+new_rectangle = Rectangle(10,15)
+print(new_rectangle.base)
+print(new_rectangle.height)
+
+  
